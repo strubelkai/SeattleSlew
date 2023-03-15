30d1a32da633c6d045b80f2a986f7b2fc9094ee4
Kai Strubel <kaistrubel@Kais-MacBook-Air.local>
2023-03-15 12:36:24 -0700

    added bootstrap to support basic css

--- SeattleSlew/__init__.py
+++ SeattleSlew/__init__.py
@@ -50,3 +50,24 @@ result = collection.update_one(
     {"patient_name": sample["patient_name"]}, {"$set": sample}, upsert=True
 )
 print("Upserted document with _id {}\n".format(result.upserted_id))
+
+from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import ContainerClient
+from azure.core.exceptions import ResourceExistsError
+
+account_name = 'seattleslew' # Must be replaced by your <storage_account_name>
+url = "https://{}.blob.core.windows.net".format(account_name)
+shared_access_key = os.environ['AZURE_STORAGE_KEY']
+
+blob_service_client = BlobServiceClient(account_url=url, credential=shared_access_key)
+
+media_container_client = blob_service_client.get_container_client("media")
+static_container_client = blob_service_client.get_container_client("static")
+
+try:
+    # [START create_container]
+    media_container_client.create_container()
+    static_container_client.create_container()
+
+except ResourceExistsError:
+            pass
