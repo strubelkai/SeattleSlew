cdfbf196a869a8e1b736278f51e4a308e2059ae3
Kai Strubel <kaistrubel@Kais-MacBook-Air.local>
2023-03-15 11:49:28 -0700

    clean up unused static files

--- SeattleSlew/__init__.py
+++ SeattleSlew/__init__.py
@@ -6,21 +6,6 @@ from random import randint
 import pymongo
 from dotenv import load_dotenv
 
-# Storage
-
-from storages.backends.azure_storage import AzureStorage
-
-class AzureMediaStorage(AzureStorage):
-    account_name = 'seattleslew' # Must be replaced by your <storage_account_name>
-    account_key = os.environ['AZURE_STORAGE_KEY'] # Must be replaced by your <storage_account_key>
-    azure_container = 'media'
-    expiration_secs = None
-
-class AzureStaticStorage(AzureStorage):
-    account_name = 'seattleslew' # Must be replaced by your storage_account_name
-    account_key = os.environ['AZURE_STORAGE_KEY'] # Must be replaced by your <storage_account_key>
-    azure_container = 'static'
-    expiration_secs = None
 
 # Create Cosmos Client and Authenticate
 
